754936bc39675caa30a3ef90a23f187dea3c40fb
Jonas Jordão de Macêdo <jonasjordao452@gmail.com>
2021-05-16 07:07:52 -0300

    feat(app): setup model & network entity

---
+++ app/src/main/java/com/jonas/payoneer/homework/model/PaymentMethod.java
@@ -0,0 +1,22 @@
+package com.jonas.payoneer.homework.model;
+
+public class PaymentMethod {
+    private String label;
+    private String logo;
+
+    public String getLabel() {
+        return label;
+    }
+
+    public void setLabel(String label) {
+        this.label = label;
+    }
+
+    public String getLogo() {
+        return logo;
+    }
+
+    public void setLogo(String logo) {
+        this.logo = logo;
+    }
+}
---
+++ app/src/main/java/com/jonas/payoneer/homework/network/entity/PaymentMethodEntity.java
@@ -0,0 +1,6 @@
+package com.jonas.payoneer.homework.network.entity;
+
+public class PaymentMethodEntity {
+    public String label;
+    public String logo;
+}
