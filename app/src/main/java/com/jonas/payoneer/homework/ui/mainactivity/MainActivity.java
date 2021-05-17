ef47f1b0a11be27a1e6c30058fa8606cd491a0a1
Jonas Jordão de Macêdo <jonasjordao452@gmail.com>
2021-05-17 08:17:23 -0300

    feat(app): setup main activity and view model

--- app/src/main/java/com/jonas/payoneer/homework/ui/mainactivity/MainActivity.java
+++ app/src/main/java/com/jonas/payoneer/homework/ui/mainactivity/MainActivity.java
@@ -1,4 +1,4 @@
-package com.jonas.payoneer.homework.ui;
+package com.jonas.payoneer.homework.ui.mainactivity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -6,6 +6,9 @@ import android.os.Bundle;
 
 import com.jonas.payoneer.homework.R;
 
+import dagger.hilt.android.AndroidEntryPoint;
+
+@AndroidEntryPoint
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -13,4 +16,5 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
 }
---
+++ app/src/main/java/com/jonas/payoneer/homework/ui/mainactivity/MainActivityViewModel.java
@@ -0,0 +1,9 @@
+package com.jonas.payoneer.homework.ui.mainactivity;
+
+import androidx.lifecycle.ViewModel;
+
+import dagger.hilt.android.lifecycle.HiltViewModel;
+
+@HiltViewModel
+public class MainActivityViewModel extends ViewModel {
+}
