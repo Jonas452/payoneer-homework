8ee7e71c2695d3285199549d02a1a08a9a8604b9
Jonas Jordão de Macêdo <jonasjordao452@gmail.com>
2021-05-20 18:36:27 -0300

    fix(app): return proper reponse in service

--- app/src/main/java/com/jonas/payoneer/homework/network/service/PaymentMethodService.java
+++ app/src/main/java/com/jonas/payoneer/homework/network/service/PaymentMethodService.java
@@ -1,15 +1,13 @@
 package com.jonas.payoneer.homework.network.service;
 
-import com.jonas.payoneer.homework.network.entity.PaymentMethodResponse;
+import com.jonas.payoneer.homework.network.entity.ListResultResponse;
 
-import java.util.List;
-
-import retrofit2.Response;
+import io.reactivex.Observable;
 import retrofit2.http.GET;
 
 public interface PaymentMethodService {
 
     @GET("optile/checkout-android/develop/shared-test/lists/listresult.json")
-    Response<List<PaymentMethodResponse>> getListPaymentMethods();
+    Observable<ListResultResponse> getListPaymentMethods();
 
 }
